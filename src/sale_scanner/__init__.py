e98ca02d1b0a91a5e4d349c86a74c6c08feee804
mainstreetmediatn-git <mainstreetmediatn@gmail.com>
2026-08-26 17:11:21 -0500

    chore: export poller pipeline and dispatcher

--- src/sale_scanner/__init__.py
+++ src/sale_scanner/__init__.py
@@ -1,2 +1,6 @@
 from .connectors import MarketplaceConnector, NormalizedListing, SearchResult
 from .ebay import EbayConfig, EbayConnector, EbayConnectorError
+from .search_models import SavedSearch, ListingChange
+from .saved_search_poller import SavedSearchPoller
+from .evaluation_pipeline import ListingEvaluationPipeline
+from .dispatcher import DispatchCandidate, QualifiedDealDispatcher
